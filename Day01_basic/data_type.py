a7ead01ef86520b68a68ab0c8cce365bec70094e
Naresh <npy.personal@gmail.com>
2025-11-21 21:13:56 +0530

    python day 1 and day 2

---
+++ Day01_basic/data_type.py
@@ -0,0 +1,16 @@
+# interger data type
+# age = 25
+# print(age)
+# print(type(age))
+
+# floating point 
+# average = 45.6
+# print(type(average))
+
+# # strings
+# fruit = 'apple is a fruit..'
+# print(fruit, type(fruit))
+
+# boolean
+water_level_flow = True 
+print(water_level_flow, type(water_level_flow))
---
+++ Day02_basic/input.py
@@ -0,0 +1,4 @@
+# input()
+
+inputValue = input()
+print(inputValue)
